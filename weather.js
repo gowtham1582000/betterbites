0cc575e9bc59ca09471369afbff8484db9313607
gowtham1582000 <gowtham1582000@gmail.com>
2023-03-20 16:50:20 +0530

    asas

--- weather.js
+++ weather.js
@@ -2,23 +2,15 @@ let weather = {
     apiKey: "366e5e489adcd459912788646cab5f97",
     fetchWeather: function () {
       fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=chennai",
-          "&units=metric&appid=" +
-          this.apiKey
+        "https://api.openweathermap.org/data/2.5/weather?q=chennai&appid=366e5e489adcd459912788646cab5f97"
       )
-      .then((response) => {
-        if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
-        }
-        return response.json();
-      })
+      
       .then((data) => this.displayWeather(data));
   },
   displayWeather: function () {
     const { temp } = data.main;
     
-    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".temp1").innerText = temp + "°C";
     
   }
  
